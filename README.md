b473343d910d2d2fea1109355f59a56c3fc37b80
Nazareth Villa <vnazareth6@gmail.com>
2022-01-02 20:49:49 -0400

    added description to README file and day 1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 100daysofcode
+
+Using 100 days of code challenge to learn and improve my skills in front-end.
+
+##### Challenge rules:
+
+- Code for at least 1 hour daily for the next 100 days starting on January 2nd, 2021.
+- Code at work does not count in the challenge.
+- tweet progress each day on twitter.
+- the progress of each day will be updated in this repository inside a file named as the number of the day.
+
+###### Daily Challenges:
+
+- Day 1:
+  - worked in a function that calculates how many secods are in an hour.
+  - Used a function that receives the radius and height of a cylinde as values, as well as using Math.PI method
--- Week one/Day-1/Day-1.js
+++ Week one/Day-1/Day-1.js
@@ -1,5 +1,5 @@
-///////1. How many seconds there are in an hour/////////////////////////////////////////
-//function 
+//1 How many seconds there are in an hour
+
 function secondsInHours(hours){
     //formula
     let seconds = hours * 3600;
@@ -9,3 +9,25 @@ function secondsInHours(hours){
 
 //call function by  getting info from user.
 secondsInHours(prompt("Write how many hours: "));
+
+
+
+
+
+//2 Cilinder Volumen 
+
+function CylinderVolumen(radius, height){
+
+    /*Volumen of a cylinder Formula: 
+        -where PI is calculated unsing the Math.PI method that way we do not have to define it. 
+        -Radius elevated by 2, to obtain the area of an circumference. 
+        -The area of a circumsference multiplied by the height is volume of a cylinder */
+    
+    const Volume = Math.PI * (radius * radius) * height;
+
+    //return value of the volumen rounded to the result to one decimal using the toFixed method.
+    return "The Volume of a cylinder is: " + Volume.toFixed(1);
+}
+
+//call function and ask the user for the 2 values using a prompt. 
+CylinderVolumen(prompt("Please, insert radius of the cylinder:"), prompt("Please, insert the height  of the cylinder"));
